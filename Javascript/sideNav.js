1e581f0c239ff0974273be7be9708e36986d5136
Dhruv Rayat <dhruvrayat50@gmail.com>
2022-02-13 13:48:24 +1300

    Side nav bar width 100%

--- Javascript/sideNav.js
+++ Javascript/sideNav.js
@@ -1,6 +1,6 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.width = "100vw";
   }
   
   /* Set the width of the side navigation to 0 */
