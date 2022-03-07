769b9d938c9de0ddc2950245fbd8a5d0c94a41e1
Dhruv Rayat <dhruvrayat50@gmail.com>
2022-03-07 14:15:31 +1300

    Commit

--- Goga python/goga.py
+++ Goga python/goga.py
@@ -1,8 +1,13 @@
 from tkinter.simpledialog import askinteger
 
 
+# Varibles
 CHILD_AGE = 13
 age = int(input("What is your age?"))
+
+# Logic
 if age < CHILD_AGE:
     print("You pay the child price!")
+
+
 print("Welcome to the zoo.")
